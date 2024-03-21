47c171fdcc60c587f83bf2b59af07fad4a24f793
Serhiy Hul <hulsehiy18@gmail.coml>
2024-03-21 08:52:55 +0200

    Добавлення деякого функціоналу в main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from star_wars_api import StarWarsApi
+from api_star_wars import StarWarsApi
 
 def print_movie_info(movie_id):
     api_client = StarWarsApi()
@@ -8,28 +8,28 @@ def print_movie_info(movie_id):
 
     print("Персонажі:")
     characters = film['characters']
-    for i, character_url in enumerate(characters, start=1):
+    for character_url in characters:
         character_data = api_client.get_entity('people', character_url.split('/')[-2])
         homeworld_data = api_client.get_entity('planets', character_data['homeworld'].split('/')[-2])
-        print(f" {i}. {character_data['name']} з планети {homeworld_data['name']}")
+        print(f" {character_data['name']} з планети {homeworld_data['name']}")
 
     print("Транспортні засоби:")
     vehicles = film['vehicles']
-    for i, vehicle_url in enumerate(vehicles, start=1):
+    for vehicle_url in vehicles:
         vehicle_data = api_client.get_entity('vehicles', vehicle_url.split('/')[-2])
-        print(f" {i}. {vehicle_data['name']}")
+        print(f" {vehicle_data['name']}")
 
     print("Космічні кораблі:")
     starships = film['starships']
-    for i, starship_url in enumerate(starships, start=1):
+    for starship_url in starships:
         starship_data = api_client.get_entity('starships', starship_url.split('/')[-2])
-        print(f" {i}. {starship_data['name']}")
+        print(f" {starship_data['name']}")
 
     print("Види істот:")
     species = film['species']
-    for i, specie_url in enumerate(species, start=1):
+    for specie_url in species:
         specie_data = api_client.get_entity('species', specie_url.split('/')[-2])
-        print(f" {i}. {specie_data['name']}")
+        print(f" {specie_data['name']}")
 
 def main():
     movie_id = input("Введіть ID фільму: ")
